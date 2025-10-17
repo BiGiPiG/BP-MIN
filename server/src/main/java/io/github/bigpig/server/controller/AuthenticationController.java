f60320bab2109b058bd923c1715a6e7153fffe6f
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-10-17 21:19:49 +0300

    refactor: add prefix /api/v1

--- server/src/main/java/io/github/bigpig/server/controller/AuthenticationController.java
+++ server/src/main/java/io/github/bigpig/server/controller/AuthenticationController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController("/api/v1")
 @RequiredArgsConstructor
 public class AuthenticationController {
 
