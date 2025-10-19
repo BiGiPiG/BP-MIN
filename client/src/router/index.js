e98e2a30a47083d5fae5fa9f7883bed279a15b0f
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-10-19 22:08:07 +0300

    feat: add router

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,8 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import LoginPage from "@/components/LoginPage.vue";
+import MainPage from "@/components/MainPage.vue";
+import { isTokenExpired } from "@/utils/jwtUtils.js";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [{
+    path: '/login',
+    name: 'Login',
+    component: LoginPage,
+    meta: { requiresAuth: false }
+  },
+    {
+      path: '/bp-min/:username',
+      name: 'BpMin', component: MainPage,
+      meta: { requiresAuth: true }
+    },
+    {
+      path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/login' }],
+  history: createWebHistory()
 })
+
+router.beforeEach((to, from, next) => {
+  const requiresAuth = to.matched.some(record =>
+      record.meta && record.meta.requiresAuth === true
+  );
+  const token = localStorage.getItem('accessToken');
+
+  if (requiresAuth && (!token || isTokenExpired(token))) {
+    next({ name: 'Login' });
+  } else if (to.name === 'Login' && token && !isTokenExpired(token)) {
+    const payload = JSON.parse(atob(token.split('.')[1].replace(/-/g, '+').replace(/_/g, '/')));
+    const username = payload.sub;
+    next({ name: 'BpMin', params: { username } });
+  } else {
+    next();
+  }
+});
 
 export default router
