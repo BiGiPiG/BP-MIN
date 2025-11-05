4224a9c1a3c27818d2adb08939d11b23cf58a07a
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-11-05 21:17:34 +0300

    refactor: rename methods and improve realisation, add logger

--- server/src/main/java/io/github/bigpig/server/controller/AuthenticationController.java
+++ server/src/main/java/io/github/bigpig/server/controller/AuthenticationController.java
@@ -6,7 +6,6 @@ import io.github.bigpig.server.dto.RegistrationRequestDto;
 import io.github.bigpig.server.service.AuthenticationService;
 import io.github.bigpig.server.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -24,39 +23,30 @@ import java.util.Map;
 public class AuthenticationController {
 
     private final AuthenticationService authenticationService;
-
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<?> register(
-            @RequestBody RegistrationRequestDto registrationDto) {
-
+    public ResponseEntity<?> signup(@RequestBody RegistrationRequestDto registrationDto) {
         if(userService.existsByUsername(registrationDto.getUsername())) {
             return ResponseEntity.badRequest().body(Map.of("message", "Username is already taken"));
         }
-
         if(userService.existsByEmail(registrationDto.getEmail())) {
             return ResponseEntity.badRequest().body(Map.of("message", "Email is already taken"));
         }
-
-        authenticationService.register(registrationDto);
-
+        log.info("Signup request received for user {}", registrationDto.getUsername());
+        authenticationService.signup(registrationDto);
         return ResponseEntity.ok(Map.of("message", "Registration was successful"));
     }
 
     @PostMapping("/signin")
-    public ResponseEntity<AuthenticationResponseDto> authenticate(
-            @RequestBody LoginRequestDto request) {
-
-        return ResponseEntity.ok(authenticationService.authenticate(request));
+    public ResponseEntity<AuthenticationResponseDto> signin(@RequestBody LoginRequestDto request) {
+        log.info("Signin request received for user {}", request.username());
+        return ResponseEntity.ok(authenticationService.signin(request));
     }
 
     @PostMapping("/refresh_token")
-    public ResponseEntity<AuthenticationResponseDto> refreshToken(
-            HttpServletRequest request,
-            HttpServletResponse response) {
-
-        log.info("🔄 Refresh token request received");
-        return authenticationService.refreshToken(request, response);
+    public ResponseEntity<AuthenticationResponseDto> refreshToken(HttpServletRequest request) {
+        log.info("Refresh token request received");
+        return authenticationService.refreshToken(request);
     }
 }
