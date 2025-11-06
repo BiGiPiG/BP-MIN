9875c93ca2860c86f1e17b3ac34fd91454670b21
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-11-06 14:11:30 +0300

    refactor: fix logic

--- server/src/main/java/io/github/bigpig/server/security/CustomLogoutHandler.java
+++ server/src/main/java/io/github/bigpig/server/security/CustomLogoutHandler.java
@@ -4,18 +4,18 @@ import io.github.bigpig.server.entity.Token;
 import io.github.bigpig.server.repository.TokenRepository;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
+@RequiredArgsConstructor
 public class CustomLogoutHandler implements LogoutHandler {
 
     private final TokenRepository tokenRepository;
-
-    public CustomLogoutHandler(TokenRepository tokenRepository) {
-        this.tokenRepository = tokenRepository;
-    }
 
     @Override
     public void logout(HttpServletRequest request,
@@ -23,18 +23,13 @@ public class CustomLogoutHandler implements LogoutHandler {
                        Authentication authentication) {
 
         String authHeader = request.getHeader("Authorization");
-
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
             return;
         }
-
         String token = authHeader.substring(7);
 
-        Token tokenEntity = tokenRepository.findByAccessToken(token).orElse(null);
+        Optional<Token> tokenEntity = tokenRepository.findByRefreshToken(token);
 
-        if (tokenEntity != null) {
-            tokenEntity.setLoggedOut(true);
-            tokenRepository.save(tokenEntity);
-        }
+        tokenEntity.ifPresent(value -> tokenRepository.deleteByUserId(value.getUser().getId()));
     }
 }
