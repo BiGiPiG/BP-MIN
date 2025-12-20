adb9d9cc5419bdcb64553a896262510a85236a8a
BiGPiG <Bogdan.ars.15@yandex.ru>
2025-12-20 21:11:31 +0300

    feat: add cors properties

--- server/src/main/java/io/github/bigpig/server/ServerApplication.java
+++ server/src/main/java/io/github/bigpig/server/ServerApplication.java
@@ -1,9 +1,12 @@
 package io.github.bigpig.server;
 
+import io.github.bigpig.server.util.CorsProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(CorsProperties.class)
 public class ServerApplication {
 
     public static void main(String[] args) {
