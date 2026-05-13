2594a2e51d58c04b2cb1fe78ea7861e1322ff6bd
BiGPiG <Bogdan.ars.15@yandex.ru>
2026-05-13 11:28:10 +0300

    refactor PresenceService

--- chat-service/src/main/java/io/github/bigpig/chatservice/service/PresenceService.java
+++ chat-service/src/main/java/io/github/bigpig/chatservice/service/PresenceService.java
@@ -4,7 +4,6 @@ import io.github.bigpig.chatservice.dto.events.UserChangedStatusEvent;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
